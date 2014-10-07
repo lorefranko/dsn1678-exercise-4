24fd18dc8e65919c793562065f22526409421f55
lorefranko <lorefranko@gmai.com>
2014-10-07 10:26:11 -0400

    shuttle working

--- js/main.js
+++ js/main.js
@@ -3,6 +3,8 @@ var $sun = $('.sun');
 var $sunSection = $('.sun-section');
 var $dipperSection = $('.dipper-section');
 var $dipper = $('.dipper');
+var $spaceShip = $('.space-ship');
+var $orangeSection = $('.orange-section');
 
 
 $win.on('scroll',function () {
@@ -10,8 +12,15 @@ $win.on('scroll',function () {
 
 	$sun.css('transform', 'rotate(' + scrollPos / 5 + 'deg)');
 	$sunSection.css('background-position', 'center ' + scrollPos / 2 + 'px')
+
+
 });
 
 $dipperSection.waypoint(function () {
   $dipper.addClass('js-dipper-fade');
 }, { offset: '50%' });
+
+$orangeSection.waypoint(function () {
+  $spaceShip.addClass('js-shuttle-fade');
+}, { offset: '50%' });
+
